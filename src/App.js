8cdca8aabda20333b432d9138247eea4c3de55b7
omkareshwar9849 <omkar.varada9849@gmail.com>
2023-05-05 02:27:16 +0530

    v61 done

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
+import Alert from './components/Alert';
 function App() {
   return (
     <>
     <NoteState>
       <Router>
         <Navbar name="iNotebook" />
+        <Alert message={"Hello! am Omkar."}/>
         <div className="container">
           <Routes>
             <Route path='/' element={<Home/>}></Route>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import NoteContext from './noteContext';
 const NoteState=(props)=>{
     const initialNotes = [
         {
-            "_id": "644d7ecb92ce78939a1fcf88",
+            "_id": "644d7ecb92ce78sd939a1fcf88",
             "user": "644cea1783de3166a93ead91",
             "title": "updated title",
             "description": "updated description",
@@ -12,7 +12,7 @@ const NoteState=(props)=>{
             "__v": 0
         },
         {
-            "_id": "644d7f29d0f1ec05db199380",
+            "_id": "644d7f29d0f1ec05dddb199380",
             "user": "644cea1783de3166a93ead91",
             "title": "My Title",
             "description": "Please wake up early",
@@ -21,7 +21,7 @@ const NoteState=(props)=>{
             "__v": 0
         },
         {
-            "_id": "645128a802097a454a380a00",
+            "_id": "645128a802097a454fda380a00",
             "user": "644cea1783de3166a93ead91",
             "title": "Omkar's note",
             "description": "Try to do this daily",
@@ -30,7 +30,7 @@ const NoteState=(props)=>{
             "__v": 0
         },
         {
-            "_id": "6451290602097a454a380a02",
+            "_id": "6451290602097a45f4a380a02",
             "user": "644cea1783de3166a93ead91",
             "title": "Prepare well for tommorrow exam",
             "description": "Should study alot for tomorrow try. should try to read all the problems present in the observation book. tomorrow sir is going to be strict there will be less chance to cheat so be prepared for that.",
@@ -43,8 +43,33 @@ const NoteState=(props)=>{
 
   const [notes, setNotes] = useState(initialNotes);
 
+  //Add a Note
+  const addNote=(title ,description ,tag)=>{
+    const note = {
+        "_id": "6451hj28a802097a454fda380a00",
+        "user": "644cea1783de3166a93ead91",
+        "title": title,
+        "description": description,
+        "tag": tag,
+        "date": "2023-05-02T15:13:44.017Z",
+        "__v": 0
+    };
+    setNotes(notes.concat(note))
+  }
+
+  //Delete a Note
+  const deleteNote=()=>{
+    
+  }
+
+
+  //Edit a Note
+  const editNote=()=>{
+    
+  }
+
   return(
-        <NoteContext.Provider value={{notes,setNotes}}>
+        <NoteContext.Provider value={{notes,addNote,deleteNote,editNote}}>
             {props.children}
         </NoteContext.Provider>
   )
