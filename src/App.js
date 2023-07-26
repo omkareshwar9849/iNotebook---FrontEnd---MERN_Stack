ac8858b327ca0cb4f452b217c53e473ba1b5153d
omkareshwar9849 <omkar.varada9849@gmail.com>
2023-07-26 18:47:42 +0530

    v70 completed

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import Home from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
 import Alert from './components/Alert';
+import Login from './components/Login';
+import Signup from './components/Signup';
 function App() {
   return (
     <>
@@ -26,6 +28,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path="/about" element={<About />}></Route>
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="/signup" element={<Signup/>}></Route>
           </Routes>
         </div>
       </Router>
